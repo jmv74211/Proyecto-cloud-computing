0d4d669d0bcdd03a5e6fb29f5ade5a255abfb8b4
Jonathan <jmv74211@gmail.com>
2018-12-02 23:44:11 +0100

    Crea modelo para acceder y manipular los datos de la BD #29

--- src/app/task_service/class/task_class.py
+++ src/app/task_service/class/task_class.py
@@ -5,11 +5,15 @@ class Task:
 
     task_num = 1
 
-    def __init__(self,_user, _task_name, _task_estimation, _task_difficulty, _task_max_date):
-        self.task_id = Task.task_id
-        Task.task_id += 1
+    def __init__(self,_user, _task_name, _task_description, _task_estimation, _task_difficulty, _task_max_date):
+        self.task_id = Task.task_num
+        Task.task_num += 1
         self.user = _user
         self.task_name = _task_name
+        self.task_description = _task_description
         self.task_estimation = _task_estimation
         self.task_difficulty = _task_difficulty
         self.task_max_date = _task_max_date
+
+    def to_dict(self):
+        return self.__dict__
