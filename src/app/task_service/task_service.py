c00b265c0a7621b17e6951b7186042566a1d0f29
Jonathan <jmv74211@gmail.com>
2019-01-13 18:52:34 +0100

    Añade host al servicio de tareas para que funcione en travis #41

--- src/app/task_service/task_service.py
+++ src/app/task_service/task_service.py
@@ -163,4 +163,4 @@ def login():
         {'WWW.Authenticate' : 'Basic realm="Login required!"'})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=3000)
+    app.run(host="0.0.0.0", port=3000, debug=True)
